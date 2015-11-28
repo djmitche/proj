a71f5790decad191192b54e5392740ef34c6ad36
Dustin J. Mitchell <dustin@mozilla.com>
2015-11-27 20:55:52 -0500

    factor out bash

--- proj/shell/shell.go
+++ proj/shell/shell.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 	"github.com/djmitche/proj/proj/config"
 	"log"
-	"os"
-	"os/exec"
-	"syscall" // TODO: don't use
 )
 
 /* shell handling */
@@ -17,61 +14,6 @@ type Shell interface {
 
 	// Actually execute the shell
 	execute() error
-}
-
-type bashShell struct {
-	rcFilename string
-	rcFile     *os.File
-}
-
-func newBashShell() (Shell, error) {
-	filename := "/tmp/proj-rcfile" // TODO
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0700)
-	if err != nil {
-		return nil, err
-	}
-	sh := &bashShell{
-		rcFilename: filename,
-		rcFile:     file,
-	}
-
-	_, err = sh.Write([]byte("[ -f ~/.bashrc ] && . ~/.bashrc\n"))
-	if err != nil {
-		return nil, err
-	}
-
-	return sh, nil
-}
-
-func (shell *bashShell) Write(p []byte) (int, error) {
-	return shell.rcFile.Write(p)
-}
-
-func (shell *bashShell) SetVariable(n, v string) error {
-	_, err := shell.rcFile.Write([]byte(fmt.Sprintf("export %s=\"%s\"\n", n, v)))
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (shell *bashShell) execute() error {
-	_, err := shell.Write([]byte(fmt.Sprintf("rm -f \"%s\"\n", shell.rcFilename)))
-	if err != nil {
-		return err
-	}
-	err = shell.rcFile.Close()
-	if err != nil {
-		return err
-	}
-
-	shellPath, err := exec.LookPath("bash")
-	if err != nil {
-		return fmt.Errorf("could not find bash: %s", err)
-	}
-
-	return syscall.Exec(shellPath,
-		[]string{shellPath, "--rcfile", shell.rcFilename, "-i"}, nil)
 }
 
 func Spawn(config *config.Config, context *Context) error {
