fc63cdf85ad4c16ac775d22e8104841d7b7ca27b
Dustin J. Mitchell <dustin@mozilla.com>
2015-11-27 22:40:33 -0500

    remove 'set -e'

--- proj/shell/bash.go
+++ proj/shell/bash.go
@@ -25,7 +25,7 @@ func newBashShell() (Shell, error) {
 		rcFile:     file,
 	}
 
-	_, err = sh.Write([]byte("[ -f ~/.bashrc ] && . ~/.bashrc\nset -e\n"))
+	_, err = sh.Write([]byte("[ -f ~/.bashrc ] && . ~/.bashrc\n"))
 	if err != nil {
 		return nil, err
 	}
